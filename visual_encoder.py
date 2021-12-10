41a56fa2a909a581f0088e04765707a4d3e25f86
MinhoPark <m.park@kaist.ac.kr>
2021-12-10 03:52:38 +0000

    resnet, timm

---
+++ visual_encoder.py
@@ -0,0 +1,27 @@
+import torch
+import timm
+
+if __name__ == '__main__':
+    # Example usage
+    num_classes = 10
+    seresnet = timm.create_model('seresnet50', True, num_classes=num_classes)
+    print(seresnet)
+    x = torch.randn((2, 3, 512, 512))  # B, C, H, W
+    out = seresnet(x)
+    print(out.shape)  # not sum-to-one
+
+    # multi-label classification use sigmoid not softmax
+    import torch.nn as nn
+    print(out)
+    print(nn.Sigmoid()(out))
+
+    # Or binary classification * N
+    seresnet = timm.create_model('seresnet50', True, num_classes=num_classes*2)
+    x = torch.randn((2, 3, 512, 512))  # B, C, H, W
+    out = seresnet(x)
+    # break by 2 then softmax
+    nn.Softmax()
+    
+
+    # then finetune ...
+    # training code
