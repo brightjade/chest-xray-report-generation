3a538479dcbbef8afced07b4c8c71a352ba66ec4
MinhoPark <m.park@kaist.ac.kr>
2021-12-15 08:28:38 +0000

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,9 @@
 import os
 import os.path as osp
+import multiprocessing as mp
+import sys
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -70,6 +73,7 @@ def write(reports, labels, output_path, verbose=False):
 
 
 def labeling(reports_path, extractor, classifier, aggregator):
+
     extract_impression = False
     reports_path = reports_path.replace('.txt', '.csv')
     output_path = reports_path.replace('.csv', '_labeled.csv')
@@ -87,14 +91,42 @@ def labeling(reports_path, extractor, classifier, aggregator):
     # Aggregate mentions to obtain one set of labels for each report.
     labels = aggregator.aggregate(loader.collection)
 
-    write(loader.reports, labels, output_path, False)
+    if labels.size != 0:
+        write(loader.reports, labels, output_path, False)
 
     pass
+
+
+def main_sub(indices, patient_dirs, extractor, classifier, aggregator):
+    error_report_path = 'error_report.txt'
+    subdir = osp.basename(osp.dirname(patient_dirs[0]))
+
+    with tqdm(desc=osp.basename(subdir), total=len(indices), ncols=100) as pbar:
+        for idx_p in indices:
+            patient_dir = patient_dirs[idx_p]
+            study_txt_list = os.listdir(patient_dir)
+            study_txt_list = [name for name in study_txt_list if '.txt' in name]
+            study_txt_paths = [osp.join(patient_dir, name) for name in study_txt_list]
+            for study_txt_path in study_txt_paths:
+                if osp.isfile(study_txt_path.replace('.txt', '_labeled.csv')):
+                    continue
+                findings = parse_findings_from_txt(study_txt_path)
+                if findings:
+                    labeling(study_txt_path, extractor, classifier, aggregator)
+                    # try:
+                    #     labeling(study_txt_path)
+                    # except:
+                    #     with open(error_report_path, 'a') as f:
+                    #         f.write(study_txt_path + '\n')
+            pbar.update(1)
 
 
 def main():
     root_dir = '/home/nas1_userE/gyuhyeonsim/physionet.org/files/mimic-cxr/2.0.0/files'
     subdirs = [osp.join(root_dir, f'p1{i}') for i in range(10)]
+    num_proc = 20
+    target_idx_subdir = int(sys.argv[1])
+    target_idx_proc = int(sys.argv[2])
     # root
     # p10 ~ p19
     # s~
@@ -119,24 +151,20 @@ def main():
     aggregator = Aggregator(CATEGORIES,
                             verbose=verbose)
 
-    for subdir in subdirs:
+    for idx_subdir, subdir in enumerate(subdirs):
+        if target_idx_subdir != idx_subdir:
+            continue
         patient_list = os.listdir(subdir)
         patient_list = [name for name in patient_list if not 'index.html' in name]
         patient_dirs = [osp.join(subdir, name) for name in patient_list]
-        with tqdm(desc=osp.basename(subdir), total=len(patient_dirs), ncols=100) as pbar:
-            for patient_dir in patient_dirs:
-                study_txt_list = os.listdir(patient_dir)
-                study_txt_list = [name for name in study_txt_list if '.txt' in name]
-                study_txt_paths = [osp.join(patient_dir, name) for name in study_txt_list]
-                for study_txt_path in study_txt_paths:
-                    findings = parse_findings_from_txt(study_txt_path)
-                    if findings:
-                        labeling(study_txt_path, extractor, classifier, aggregator)
-                pbar.update(1)
 
-    pass
+        total_file_len = len(patient_dirs)
+        indices = np.arange(total_file_len)
+        split_array = np.array_split(indices, num_proc)
+
+        main_sub(split_array[target_idx_proc], patient_dirs,
+                 extractor, classifier, aggregator)
 
 
 if __name__ == '__main__':
-
     main()
