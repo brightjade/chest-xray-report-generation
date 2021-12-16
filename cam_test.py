14b401e28e57822fdf3dc060d2bd0c1e7362c322
MinhoPark <m.park@kaist.ac.kr>
2021-12-16 16:36:51 +0000

    cam R.I.P.

---
+++ cam_test.py
@@ -0,0 +1,35 @@
+import numpy as np
+from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
+import torch
+from torchvision.models import resnet50
+from PIL import Image
+import torchvision
+model = resnet50(pretrained=True)
+target_layers = [model.layer4[-1]]
+input_tensor = Image.open('dog_cat.jpg')  # Create an input tensor image for your model..
+input_tensor = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])(input_tensor).unsqueeze(0)
+# Note: input_tensor can be a batch tensor with several images!
+
+# Construct the CAM object once, and then re-use it on many images:
+cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
+
+# You can also use it within a with statement, to make sure it is freed,
+# In case you need to re-create it inside an outer loop:
+# with GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda) as cam:
+#   ...
+
+# If target_category is None, the highest scoring category
+# will be used for every image in the batch.
+# target_category can also be an integer, or a list of different integers
+# for every image in the batch.
+target_category = 281
+
+# You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+# In this example grayscale_cam has only one image in the batch:
+grayscale_cam = grayscale_cam[0, :]
+rgb_img = np.array(input_tensor[0]).transpose(1, 2, 0)
+visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+Image.fromarray(visualization).save('1.jpg')
